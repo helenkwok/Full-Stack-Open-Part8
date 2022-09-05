ae21e5ccbb5ce7e3d12294653a1361a9fc77c4fa
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-09-05 15:50:17 +0930

    8.5: Books by genre

--- library-backend/index.js
+++ library-backend/index.js
@@ -109,7 +109,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -119,10 +119,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (args.author) {
-        return books.filter(b => b.author === args.author)
+      let filteredBooks = books
+      if (args) {
+        if (args.author) filteredBooks = filteredBooks.filter(b => b.author === args.author)
+        if (args.genre) filteredBooks = filteredBooks.filter(b => b.genres.includes(args.genre))
+        return filteredBooks
       } else {
-        return books
+        return filteredBooks
     }},
     allAuthors: () => authors
   },
