1d2e049741152cfd8a6a99c5802058aab6ace589
helenkwok <107752876+helenkwok@users.noreply.github.com>
2022-09-09 13:06:10 +0930

    8.10: Adding a book

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,10 +1,19 @@
 import { useState } from 'react'
+import Notify from './components/Notify'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
   const [page, setPage] = useState('authors')
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
 
   return (
     <div>
@@ -14,11 +23,13 @@ const App = () => {
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
+      <Notify errorMessage={errorMessage} />
+
       <Authors show={page === 'authors'} />
 
       <Books show={page === 'books'} />
 
-      <NewBook show={page === 'add'} />
+      <NewBook show={page === 'add'} setError={notify} />
     </div>
   )
 }
